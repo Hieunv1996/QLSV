a46a4f8b3807b8daaa52996adb8f8b7ed75b9c94
Hieu Nguyen <vanhieu2910@gmail.com>
2016-10-29 12:33:26 +0700

    2910199612

---
+++ QLSV.Data/clsSinhVien.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSV.Data
+{
+    class clsSinhVien
+    {
+        private string _idSinhVien;
+        private string _hoTen;
+        private string _ngaySinh;
+        private string _gioiTinh;
+        private string _diaChi;
+        private string _idLop;
+
+        public string idSinhvien { get { return _idSinhVien; } set { _idSinhVien = value; } }
+        public string hoTen { get { return _hoTen; } set { _hoTen = value; } }
+        public string ngaySinh { get { return _ngaySinh; } set { _ngaySinh = value; } }
+        public string diaChi { get { return _diaChi; } set { _diaChi = value; } }
+        public string idLop { get { return _idLop; } set { _idLop = value; } }
+
+
+    }
+}
