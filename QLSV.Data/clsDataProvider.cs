41f74578c99e170e8a545e48d78f535dcfa1fb88
Hieu Nguyen <vanhieu2910@gmail.com>
2016-10-29 12:14:29 +0700

    29101996

--- QLSV.Data/clsDataProvider.cs
+++ QLSV.Data/clsDataProvider.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data;
+using System.Data.SqlClient;
 namespace QLSV.Data
 {
     class clsDataProvider
     {
+        private static string connect = "Data Source=(local);Initial Catalog=QLSV;Integrated Security=True";
+        private static SqlConnection conn;
+
+        public clsDataProvider()
+        {
+            if (conn == null)
+                conn = new SqlConnection(connect);
+        }
+
+
     }
 }
